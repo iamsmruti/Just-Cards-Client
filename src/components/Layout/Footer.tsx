d254629d3c3f7d233b14e898d1eed370ee75d97b
Keanenb <keanenbafekr@me.com>
2023-12-11 17:53:29 -0500

    Added a footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,20 @@
+import './Footer.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="footer-content">
+        <p className='text-bluish'>Just Flashcards © {new Date().getFullYear()}</p>
+        <br></br>
+        <p>Seamlessly create, refine, and structure your Flash Cards into decks and memorize.</p>
+        <br></br>
+        <ul className="footer-links text-reddish">
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+          <li><a href="https://github.com/iamsmruti/Just-Cards-Client" target="_blank">Github</a></li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
